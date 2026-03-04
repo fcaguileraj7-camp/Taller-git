ee248b9dd400b6501abff480a02e3e534fba9162
alberthsantiagoleal-cmyk <alberthsantiagoleal@gmail.com>
2026-03-04 16:39:20 -0500

    feat:agregue cuerpo de la funcion

--- usuario_b.py
+++ usuario_b.py
@@ -1,2 +1,12 @@
 def primos_en_rango(inicio, fin):
     def es_primo(n):
+        if n < 2:
+            return False
+        if n == 2:
+            return True
+        if n % 2 == 0:
+            return False
+        for i in range(3, int(n**0.5) + 1, 2):
+            if n % i == 0:
+                return False
+        return True
