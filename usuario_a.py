3c43820d806b66b3247b7b55ec3a0da0e089adc1
fcaguileraj7 <fcaguileraj7@gmail.com>
2026-03-04 22:09:57 -0500

    feat: crear función capicua

--- usuario_a.py
+++ usuario_a.py
@@ -15,3 +15,11 @@ def fibonacci():
         a, b = b, a + b
 
     print()
+
+def capicua():
+    numero = input("Ingrese un número: ")
+
+    if numero == numero[::-1]:
+        print("El número es capicúa")
+    else:
+        print("El número no es capicúa")
